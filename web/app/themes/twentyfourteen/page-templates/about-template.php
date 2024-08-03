bb1e6269c7341a03030b68206a271a0eaaad8a0b
Ramzy Berdio <rberdio@gmail.com>
2024-08-03 13:56:14 +0700

    ramzi/feat--halaman-about

--- web/app/themes/twentyfourteen/page-templates/about-template.php
+++ web/app/themes/twentyfourteen/page-templates/about-template.php
@@ -7,12 +7,72 @@
 get_header('main');
 ?>
 
+<section class="et-breadcrumb bg-[#000D83] pt-[210px] lg:pt-[190px] sm:pt-[160px] pb-[130px] lg:pb-[110px] sm:pb-[80px] relative z-[1] before:absolute before:inset-0 before:bg-[url('assets/img/breadcrumb-bg.jpg')] before:bg-no-repeat before:bg-cover before:bg-center before:-z-[1] before:opacity-30">
+    <div class="container mx-auto max-w-[1200px] px-[12px] xl:max-w-full text-center text-white">
+        <h1 class="et-breadcrumb-title font-medium text-[56px] md:text-[50px] xs:text-[45px]">About Bethany</h1>
+        <ul class="inline-flex items-center gap-[10px] font-medium text-[16px]">
+            <li class="opacity-80"><a href="index-2.html" class="hover:text-etBlue">Home</a></li>
+            <li><i class="fa-solid fa-angle-right"></i><i class="fa-solid fa-angle-right"></i></li>
+            <li class="current-page">About Bethany</li>
+        </ul>
+    </div>
+</section>
+
+<section class="et-about py-[130px] xl:py-[80px] md:py-[60px] overflow-hidden relative">
+    <div class="container mx-auto max-w-[calc(100%-39.1vw)] xxxl:max-w-[calc(100%-350px)] xl:max-w-[calc(100%-170px)] px-[12px] lg:max-w-full">
+        <div class="flex items-center md:flex-wrap gap-x-[60px] xxl:gap-x-[40px] lg:gap-x-[30px] gap-y-[40px] sm:gap-y-[40px] lg:justify-center">
+            <!-- left -->
+            <div class="et-about-img relative z-[1] md:w-auto shrink-0 max-w-full mr-[70px] md:mr-0 ml-[28px] xxxl:ml-[47px] md:ml-0 xs:w-[70%] xxs:w-[90%] xxs:ml-[47px]">
+                <img src="<?= get_template_directory_uri()?>/assets/img/about-img.jpg" alt="image" class="shrink-0 rounded-[10px]" />
+                <img src="<?= get_template_directory_uri()?>/assets/img/about-img-3.jpg" alt="image" class="et-about-floating-img absolute top-[45px] -right-[70px] shrink-0 rounded-[20px] border-white border-[10px] xs:hidden" />
+
+                <!-- vectors -->
+                <div class="et-about-vectors xs:hidden">
+                    <img src="<?= get_template_directory_uri()?>/assets/img/about-img-vector-1.png" alt="vector" class="et-about-vector-1 absolute -left-[47px] top-[20px] -z-[1]" />
+                    <img src="<?= get_template_directory_uri()?>/assets/img/about-img-vector-2.png" alt="vector" class="et-about-vector-2 absolute -left-[27px] top-[41px] -z-[1]" />
+                    <img src="<?= get_template_directory_uri()?>/assets/img/about-img-vector-3.png" alt="vector" class="et-about-vector absolute -right-[24px] bottom-[34px] -z-[1]" />
+                </div>
+
+                <!-- video btn -->
+                <div class="absolute bottom-[50px] -left-[47px] lg:-left-[27px] md:-left-[47px] w-[180px] lg:w-[160px] aspect-square bg-[url('assets/img/about-video-btn-bg.jpg')] text-center text-[22px] text-white z-[1] flex items-center justify-center before:absolute before:bg-etBlue/80 before:-z-[1] before:inset-0 after:bg-etBlack after:w-[47px] after:h-[30px] after:absolute after:top-[100%] after:left-0 after:-z-[2] after:skew-y-[30deg] after:-translate-y-[17px]">
+                    <a href="https://www.youtube.com/watch?v=AQleI8oFqZo&amp;t=1s" data-fslightbox="about-video" class="w-[107px] aspect-square rounded-full border border-white/20 flex justify-center items-center text-etBlue relative z-[1] text-[18px] before:absolute before:w-[56px] before:h-[56px] before:bg-white before:rounded-full before:-z-[1] hover:text-black"><i class="fa-solid fa-play"></i></a>
+                </div>
+            </div>
+
+            <!-- right -->
+            <div class="et-about__txt">
+                <h6 class="et-section-sub-title anim-text">Vision Bethany</h6>
+                <h2 class="et-section-title mb-[24px] md:mb-[19px] anim-text" style="font-size: 25px;">Menjadi Komunitas Profesional Muda yang cinta Tuhan dan diberkati untuk memberkati.</h2>
+
+                <div class="rev-slide-up">
+                    <!-- txt -->
+                    <div class="my-[30px]">
+                        <h6 class="et-section-sub-title anim-text">Value Bethany</h6>
+                        <h2 class="et-section-title mb-[24px] md:mb-[19px] anim-text" style="font-size: 25px;">U PRO IN HIM</h2>
+                        <p class="my-[10px] text-[15px]"><i>Unity</i> (memelihara kesatuan dan saling mendukung dimanapun kita berada)</p>
+                        <p class="my-[10px] text-[15px]"><i>Professional</i> (menjadi spesialis yang terdidik, berkualitas, dan tidak murahan dalam bidang yang Tuhan percayakan)</p>
+                        <p class="my-[10px] text-[15px]"><i>Integrity</i> (memiliki keteguhan hati, selalu konsisten, memiliki sikap yang sama ketika ada yang memperhatikan maupun saat tidak ada yang memperhatikan)</p>
+                        <p class="my-[10px] text-[15px]"><i>Humility</i> (memiliki sifat tingkah laku yang rendah hati dalam setiap kondisi)</p>
+                        <p class="my-[10px] text-[15px]"><i>Impact</i> (menjadi dampak, role model, membuat perbedaan dan memberi pengaruh yang positif dimanapun kita berada)</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- vectors -->
+    <div class="xs:hidden">
+        <img src="<?= get_template_directory_uri()?>/assets/img/about-vector-1.png" alt="vector" class="pointer-events-none absolute top-[340px] left-[90px] -z-[1]">
+        <img src="<?= get_template_directory_uri()?>/assets/img/about-vector-2.png" alt="vector" class="pointer-events-none absolute top-[153px] right-0 -z-[1]">
+    </div>
+</section>
+
 <!-- HISTORY SECTION START -->
 <section class="bg-[#001B52] relative z-[1] before:absolute before:inset-0 before:-z-[1] before:bg-[url('assets/img/history-bg.jpg')] before:bg-no-repeat before:bg-cover before:bg-center before:mix-blend-screen before:opacity-25 py-[130px] lg:py-[80px] md:py-[60px]">
     <div class="container mx-auto max-w-[1200px] px-[12px] xl:max-w-full">
         <!-- heading -->
         <div class="et-upcoming-events-heading text-center max-w-[60%] mx-auto mb-[52px] xl:mb-[42px] lg:mb-[32px] md:max-w-[70%] sm:max-w-[80%] xs:max-w-full">
-            <h6 class="et-section-sub-title !text-white">Company History</h6>
+            <h6 class="et-section-sub-title !text-white">Bethany Professional</h6>
             <h2 class="et-section-title !text-white">Our Bethany Internasal Design Conferences History</h2>
         </div>
 
