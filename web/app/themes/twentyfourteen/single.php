08bd8d83c59a5d8c7237bf9b0f837de74ee5447d
Radinal Dewantara <radinaldewantara@gmail.com>
2024-08-03 11:54:44 +0700

    Add single.php example

--- web/app/themes/twentyfourteen/single.php
+++ web/app/themes/twentyfourteen/single.php
@@ -1,15 +1,12 @@
 <?php
-// Ensure you have access to the global post object
 global $post;
 
-// Get the post type of the current post
 $post_type = get_post_type($post);
 
-// Check if the post type is 'submission'
 if ($post_type == 'submission') {
-	// Include the custom template for 'submission'
 	include(get_template_directory() . '/page-templates/submission-template.php');
+} else if (in_category('1')) { // THIS IS ID EXAMPLE FOR CATEGORY ID
+	include(get_template_directory() . '/page-templates/single-default.php');
 } else {
-	// Include the default single post template
 	include(get_template_directory() . '/page-templates/single-default.php');
 }
