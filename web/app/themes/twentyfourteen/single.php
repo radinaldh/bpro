5ee3b23fc9fca83ded709e17fa84c5cd6209f6ce
Radinal Dewantara <radinaldewantara@gmail.com>
2024-08-08 21:20:27 +0700

    Fix single.php

--- web/app/themes/twentyfourteen/single.php
+++ web/app/themes/twentyfourteen/single.php
@@ -3,14 +3,9 @@ global $post;
 
 $post_type = get_post_type($post);
 
-// if ($post_type == 'submission') {
-// 	include(get_template_directory() . '/page-templates/submission-template.php');
-// } else if (in_category('1')) { // THIS IS ID EXAMPLE FOR CATEGORY ID
-// 	include(get_template_directory() . '/page-templates/single-default.php');
-// } else {
-// 	include(get_template_directory() . '/page-templates/single-default.php');
-// } 
-
+if ($post_type == 'submission') {
+	include(get_template_directory() . '/page-templates/submission-template.php');
+}
 if (in_category('3')) {
-	include(TEMPLATEPATH . '/page-templates/journal-detail-template.php');
+	include(get_template_directory() . '/page-templates/journal-detail-template.php');
 }
