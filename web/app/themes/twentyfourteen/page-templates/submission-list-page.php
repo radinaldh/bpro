52d19c90226a835915a749463fc3f6e68921db22
Radinal Dewantara <radinaldewantara@gmail.com>
2024-08-08 22:16:23 +0700

    Add script inline for front end

--- web/app/themes/twentyfourteen/page-templates/submission-list-page.php
+++ web/app/themes/twentyfourteen/page-templates/submission-list-page.php
@@ -86,6 +86,32 @@ get_header();
     </div>
 </div>
 
+<script>
+    jQuery(document).ready(function($) {
+        $(".send-email").on("click", function() {
+            var post_id = $(this).data("post-id");
+            var $button = $(this);
+
+            $.ajax({
+                url: ajax_params.ajax_url,
+                type: "POST",
+                data: {
+                    action: "send_email",
+                    post_id: post_id,
+                },
+                success: function(response) {
+                    if (response.success) {
+                        alert("Email sent successfully.");
+                        $button.text("Send Email Again");
+                    } else {
+                        alert("Failed to send email.");
+                    }
+                },
+            });
+        });
+    });
+</script>
+
 <?php
 get_footer();
 ?>
