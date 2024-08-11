9b0dd8b28ac43f6cf8c3d5ebc3cd0af71ac7dc1f
Ramzy Berdio <rberdio@gmail.com>
2024-08-11 14:52:44 +0700

    update

--- web/app/themes/twentyfourteen/page-templates/makalah-mingguan.php
+++ web/app/themes/twentyfourteen/page-templates/makalah-mingguan.php
@@ -18,6 +18,16 @@ get_header('main'); ?>
 .et-single-accordion-item__body a {
     font-size: 14px;
 }
+
+
+.hero-banner::before {
+    content: '';
+    background-color: rgb(0 13 131 / 0.7);
+    height: 100%;
+    width: 100%;
+    top: 0;
+    bottom: 0;
+}
 </style>
 
 
@@ -25,8 +35,7 @@ get_header('main'); ?>
 <!-- BREADCRUMB SECTION START -->
 <?php if ( have_rows( 'section_header' ) ) : ?>
 <?php while ( have_rows( 'section_header' ) ) : the_row(); ?>
-<section style="background-image:url('<?php the_sub_field( 'background_image' ); ?>'); background-size:cover; background-position:center" 
-    class="et-breadcrumb bg-[#000D83] pt-[210px] lg:pt-[190px] sm:pt-[160px] pb-[130px] lg:pb-[110px] sm:pb-[80px] relative z-[1] before:absolute before:inset-0 before:bg-[url('<?= get_template_directory_uri(); ?>/assets/img/breadcrumb-bg.jpg')] before:bg-no-repeat before:bg-cover before:bg-center before:-z-[1] before:opacity-30">
+<section style="background-image:url('<?php the_sub_field( 'background_image' ); ?>'); background-size:cover; background-position:center" class="hero-banner et-breadcrumb pt-[210px] lg:pt-[190px] sm:pt-[160px] pb-[130px] lg:pb-[110px] sm:pb-[80px] relative z-[1] before:absolute before:inset-0 before:bg-[url('<?= get_template_directory_uri(); ?>/assets/img/breadcrumb-bg.jpg')] before:bg-no-repeat before:bg-cover before:bg-center before:-z-[1] before:opacity-30">
     <div class="container mx-auto max-w-[1200px] px-[12px] xl:max-w-full text-center text-white">
         <h1 class="et-breadcrumb-title font-medium text-[56px] md:text-[50px] sm:text-[22px]">
             <?php the_sub_field( 'title' ); ?></h1>
