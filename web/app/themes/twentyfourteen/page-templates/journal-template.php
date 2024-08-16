35373f7df47eeec6d6f25e807f7e8807f8f0c49b
Nurholis <nrhlsmjd@gmail.com>
2024-08-16 15:05:20 +0700

    update journal

--- web/app/themes/twentyfourteen/page-templates/journal-template.php
+++ web/app/themes/twentyfourteen/page-templates/journal-template.php
@@ -125,7 +125,7 @@ get_header('main');
                     'category_name' => 'Journal',
                     'orderby'    => 'date',
                     'post_status' => 'publish',
-                    'posts_per_page' => 2,
+                    'posts_per_page' => 3,
                     'paged' => $currentPage,
                 );
                 $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
